ee27ef7da286733fcacd7b5469116dbbc77b0e20
Mathieu Lajugie <mathieu@caring.com>
2013-09-01 11:29:24 +0200

    test websocket router macro

--- test/dynamo/router/base_test.exs
+++ test/dynamo/router/base_test.exs
@@ -61,11 +61,14 @@ defmodule Dynamo.Router.BaseTest do
     end
   end
 
+
   defmodule RootSample do
     use Dynamo.Router
 
     prepare do: conn.fetch(:params)
 
+    defmodule WebsocketHandler1 do; end
+    websocket "/ws", using: WebsocketHandler1
     forward "/", to: Sample1
   end
 
@@ -173,5 +176,10 @@ defmodule Dynamo.Router.BaseTest do
     assert conn.assigns[:value] == :root
     assert conn.route_params[:var] == "hello"
     assert conn.params[:var] == "hello"
+  end
+
+  test "forwarding to a websocket handler" do
+    conn = get("/ws")
+    assert conn.websocket_handler == RootSample.WebsocketHandler1
   end
 end
